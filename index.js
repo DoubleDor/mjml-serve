56a3fa7adb8335577b887365001f56c16070b31a
Michael Lyons <mdl0394@gmail.com>
2016-02-23 15:47:34 -0800

    Added index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+/* index.js -- main script file for mjml-server
+ *
+ * Copyright (C) 2016 Dor Technologies
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+var fs = require( 'fs' ),
+    path = require( 'path' );
+
+var express = require( 'express' ),
+    mjml = require( 'mjml' );
+
+var app = express();
+
+app.get( '/:filename', function( req, res ) {
+    var mjml_file = fs.readFileSync( path.resolve( __dirname, req.params.filename ) ).toString();
+    var html_file = mjml.mjml2html( mjml_file );
+    res.send( html_file );
+} );
+
+app.listen( 5432, function( err ) {
+    console.log( 'Server Started. View at: http://localhost:5432/<filename.mjml>' );
+} );
