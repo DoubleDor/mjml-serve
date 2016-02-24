8ecf79b7374352ef82b6806d9be50c8e5f9b9e5b
Michael Lyons <mdl0394@gmail.com>
2016-02-23 16:08:48 -0800

    Added ability to specify root dir as an argument

--- index.js
+++ index.js
@@ -16,8 +16,19 @@ var express = require( 'express' ),
 
 var app = express();
 
+var base_dir = process.cwd();
+
+if( process.argv.length === 3 ) {
+    console.log( process.argv );
+    if( path.isAbsolute( process.argv[ 2 ] ) ) {
+        base_dir = process.argv[ 2 ];
+    } else {
+        base_dir = path.join( process.cwd(), process.argv[ 2 ] );
+    }
+}
+
 app.get( '/:filename', function( req, res ) {
-    var mjml_file = fs.readFileSync( path.resolve( process.cwd(), req.params.filename ) ).toString();
+    var mjml_file = fs.readFileSync( path.resolve( base_dir, req.params.filename ) ).toString();
     var html_file = mjml.mjml2html( mjml_file );
     res.send( html_file );
 } );
